93e7f6afbcd3b46805343aff41e9ef27166d236a
github eunbiro <ebee91@naver.com>
2022-12-23 21:50:20 +0900

    up

---
+++ src/main/java/DTO/Hosp.java
@@ -0,0 +1,29 @@
+package DTO;
+
+public class Hosp {
+	String hospcode;
+	String hospname;
+	String count;
+	
+	
+	public String getHospcode() {
+		return hospcode;
+	}
+	public void setHospcode(String hospcode) {
+		this.hospcode = hospcode;
+	}
+	public String getHospname() {
+		return hospname;
+	}
+	public void setHospname(String hospname) {
+		this.hospname = hospname;
+	}
+	public String getCount() {
+		return count;
+	}
+	public void setCount(String count) {
+		this.count = count;
+	}
+	
+	
+}
---
+++ src/main/java/DTO/Vaccresv.java
@@ -0,0 +1,93 @@
+package DTO;
+
+public class Vaccresv {
+	String resvno;
+	String jumin;
+	String vcode;
+	String hcode;
+	String resvdate;
+	String resvtime;
+	String pname;
+	String gender;
+	String tel;
+	String hospname;
+	String hosptel;
+	String hospaddr;
+	
+	
+	
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getHospname() {
+		return hospname;
+	}
+	public void setHospname(String hospname) {
+		this.hospname = hospname;
+	}
+	public String getHosptel() {
+		return hosptel;
+	}
+	public void setHosptel(String hosptel) {
+		this.hosptel = hosptel;
+	}
+	public String getHospaddr() {
+		return hospaddr;
+	}
+	public void setHospaddr(String hospaddr) {
+		this.hospaddr = hospaddr;
+	}
+	public String getResvno() {
+		return resvno;
+	}
+	public void setResvno(String resvno) {
+		this.resvno = resvno;
+	}
+	public String getJumin() {
+		return jumin;
+	}
+	public void setJumin(String jumin) {
+		this.jumin = jumin;
+	}
+	public String getVcode() {
+		return vcode;
+	}
+	public void setVcode(String vcode) {
+		this.vcode = vcode;
+	}
+	public String getHcode() {
+		return hcode;
+	}
+	public void setHcode(String hcode) {
+		this.hcode = hcode;
+	}
+	public String getResvdate() {
+		return resvdate;
+	}
+	public void setResvdate(String resvdate) {
+		this.resvdate = resvdate;
+	}
+	public String getResvtime() {
+		return resvtime;
+	}
+	public void setResvtime(String resvtime) {
+		this.resvtime = resvtime;
+	}
+	
+	
+}
